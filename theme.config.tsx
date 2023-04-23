ee5f6697c9e817bdc9307659d0d85de8e4456639
Rintcius Blok <rintcius@gmail.com>
2023-04-23 14:18:32 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Logo</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/rintcius/rintcius',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/rintcius/rintcius',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Footer',
   },
 }
 
